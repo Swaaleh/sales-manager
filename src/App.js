36500d079e9a5093d2e8e6c9669f7f1dc63b64c4
Swaleh Mhina <mhinasw@gmail.com>
2025-02-22 14:00:46 +0300

    Using salesList in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
 import Product from "./components/Product";
 import Counter from "./components/Counter";
+import SalesList from "./components/SalesList";
+import { useState } from "react";
 function App(){
+  const [sales, setSales] = useState(["Laptop", "Mobile", "Tablet"]);
   return(
     <div>
       <Header/>
+      <SalesList sales={sales}/>
       <Product name="Laptop" price="500"/>
       <Product name="Mobile" price="200"/>
       <Counter/>
